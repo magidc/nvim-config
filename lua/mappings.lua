06d17408aa4c8fce8407556067a9c568e039533b
Ricardo Rodriguez <ricardo@reportingstandard.com>
2023-01-18 13:50:25 +0100

    config_snippets

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -212,3 +212,4 @@ wk.register({
 
 
 wk.setup {}
+
--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -50,6 +50,30 @@ cmp.setup {
     },
 
     mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+                -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                -- they way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),

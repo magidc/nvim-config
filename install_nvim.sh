772d2f1585bebc64c6c938a60ead4c98f0e50b88
Ricardo Rodriguez <ricardo@reportingstandard.com>
2023-12-31 00:10:32 +0100

    fix install script

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 sudo apt -y install git ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-git clone https://github.com/neovim/neovim
-cd neovim 
+git clone https://github.com/neovim/neovim nvim_install
+cd nvim_install
 git checkout stable
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 cd ..
-rm -rf nvim
+rm -rf nvim_install

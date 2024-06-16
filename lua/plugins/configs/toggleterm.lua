a902b7aa02b52f53c669cd11d2674542fdd0d04d
Venkata Subramani Renduchintala <rvs@Macbook-Pro-M3-Max.local>
2024-06-16 03:10:36 +0200

    Toggle Term configuration

--- lua/plugins/configs/toggleterm.lua
+++ lua/plugins/configs/toggleterm.lua
@@ -5,8 +5,8 @@ return {
 	config = function()
 		require("toggleterm").setup({
 			-- size can be a number or function which is passed the current terminal
-			--size = 20 |
-			size = function(term)
+			-- size = 20 | 
+      size = function(term)
 				if term.direction == "horizontal" then
 					return 15
 				elseif term.direction == "vertical" then
@@ -15,10 +15,40 @@ return {
 			end,
 			open_mapping = [[<leader>vt]],
 			insert_mappings = false,
+      start_in_insert = true,
 			--direction = 'vertical' | 'horizontal' | 'tab' | 'float',
-			direction = "float",
+			direction = 'float',
 			close_on_exit = true, -- close the terminal window when the process exits
-			shell = "/usr/bin/zsh", -- change the default shell
-		})
+			-- shell = "/bin/zsh", -- change the default shell
+      shell = vim.o.shell,
+      persist_size = true,
+      persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+      -- This field is only relevant if direction is set to 'float'
+      float_opts = {
+        -- The border key is *almost* the same as 'nvim_open_win'
+        -- see :h nvim_open_win for details on borders however
+        -- the 'curved' border is a custom border type
+        -- not natively supported but implemented in this plugin.
+        -- border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+        border = 'single',
+        -- like `size`, width, height, row, and col can be a number or function which is passed the current terminal
+        -- [[ 
+        -- width = <value>,
+        -- height = <value>,
+        -- row = <value>,
+        -- col = <value>,
+        -- winblend = 3,
+        -- zindex = <value>, 
+        -- ]]
+        -- title_pos = 'left' | 'center' | 'right', position of the title of the floating window
+        title_pos = 'left',
+      },
+      winbar = {
+        enabled = false,
+        name_formatter = function(term) --  term: Terminal
+          return term.name
+        end
+      },    
+    })
 	end,
 }

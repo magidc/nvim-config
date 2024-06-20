15faece179cead11feba9a59497f247db8a27ea2
Venkata Subramani Renduchintala <rvs@Macbook-Pro-M3-Max.local>
2024-06-20 14:12:34 +0200

    LuaSnip update

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -8,7 +8,7 @@ return {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
-		-- "hrsh7th/cmp-nvim-lsp-document-symbol",
+		"hrsh7th/cmp-nvim-lsp-document-symbol",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
 		"lukas-reineke/cmp-under-comparator", -- Better sort completion items starting with underscore (Python)

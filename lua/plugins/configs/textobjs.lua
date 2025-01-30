1f859fd1ff76fa89a107bd20201dba58aa1a005d
magidc <ricardo.rodcas@gmail.com>
2025-01-30 08:54:20 +0100

    various_objs_update

--- lua/plugins/configs/textobjs.lua
+++ lua/plugins/configs/textobjs.lua
@@ -1,19 +1,20 @@
 return {
-		"chrisgrieser/nvim-various-textobjs",
-		config = function()
-			require("various-textobjs").setup({
+	"chrisgrieser/nvim-various-textobjs",
+	config = function()
+		require("various-textobjs").setup({
+			forwardLooking = {
 				-- lines to seek forwards for "small" textobjs (mostly characterwise textobjs)
 				-- set to 0 to only look in the current line
-				lookForwardSmall = 5,
-
+				small = 5,
 				-- lines to seek forwards for "big" textobjs (mostly linewise textobjs)
-				lookForwardBig = 15,
-
+				big = 15,
+			},
+			keymaps = {
 				-- use suggested keymaps (see README)
-				useDefaultKeymaps = true,
-
+				useDefaults = true,
 				-- disable some default keymaps, e.g. { "ai", "ii" }
-				disabledKeymaps = {},
-			})
-		end,
-	}
+				disabledDefaults = {},
+			},
+		})
+	end,
+}

7c753395c76357a6631edac1ed1c42358a6c5e53
Venkata Subramani Renduchintala <venkatasubramani@protonmail.ch>
2025-04-28 01:13:43 +0200

    Config Update - warning removal

--- lua/plugins/configs/textobjs.lua
+++ lua/plugins/configs/textobjs.lua
@@ -2,18 +2,27 @@ return {
 		"chrisgrieser/nvim-various-textobjs",
 		config = function()
 			require("various-textobjs").setup({
+        keymaps = {
+          -- See overview table in README for the defaults. (Note that lazy-loading
+          -- this plugin, the default keymaps cannot be set up. if you set this to
+          -- `true`, you thus need to add `lazy = false` to your lazy.nvim config.)
+          useDefaults = true,
+
+          -- disable only some default keymaps, for example { "ai", "!" }
+          -- (only relevant when you set `useDefaults = true`)
+          ---@type string[]
+          disabledDefaults = {},
+        },
+
 				-- lines to seek forwards for "small" textobjs (mostly characterwise textobjs)
 				-- set to 0 to only look in the current line
-				lookForwardSmall = 5,
-
-				-- lines to seek forwards for "big" textobjs (mostly linewise textobjs)
-				lookForwardBig = 15,
-
-				-- use suggested keymaps (see README)
-				useDefaultKeymaps = true,
-
-				-- disable some default keymaps, e.g. { "ai", "ii" }
-				disabledKeymaps = {},
+        forwardLooking = {
+          -- Number of lines to seek forwards for a text object. See the overview
+          -- table in the README for which text object uses which value.
+          small = 5,
+          -- lines to seek forwards for "big" textobjs (mostly linewise textobjs)
+          big = 15,
+        },
 			})
 		end,
 	}
